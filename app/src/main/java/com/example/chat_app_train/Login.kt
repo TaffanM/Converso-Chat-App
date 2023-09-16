e4cf36bbf25684817a411cd06a333a68cb5aa097
Taffan M <75323709+TaffanM@users.noreply.github.com>
2023-09-16 19:32:10 +0700

    Adding firebase auth

--- app/src/main/java/com/example/chat_app_train/Login.kt
+++ app/src/main/java/com/example/chat_app_train/Login.kt
@@ -1,17 +1,66 @@
 package com.example.chat_app_train
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignIn: Button
+    private lateinit var tvSignUp: TextView
+    private lateinit var mAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.inputEmail)
+        edtPassword = findViewById(R.id.inputPassword)
+        btnSignIn = findViewById(R.id.button)
+        tvSignUp = findViewById(R.id.textViewSignUp)
+
+        btnSignIn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                login(email, password)
+            } else {
+                Toast.makeText(this@Login, "Please fill the username and the password", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful ){
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "Either the username or password is incorrect", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
