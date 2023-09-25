b639079986dab7e7114777470cb824caf2676ab8
Taffan M <75323709+TaffanM@users.noreply.github.com>
2023-09-26 00:35:38 +0700

    Add password toggle visibility

--- app/src/main/java/com/example/chat_app_train/Login.kt
+++ app/src/main/java/com/example/chat_app_train/Login.kt
@@ -3,12 +3,16 @@ package com.example.chat_app_train
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -19,6 +23,7 @@ class Login : AppCompatActivity() {
     private lateinit var btnSignIn: Button
     private lateinit var tvSignUp: TextView
     private lateinit var mAuth: FirebaseAuth
+    private var isPasswordVisible = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +39,10 @@ class Login : AppCompatActivity() {
         edtPassword = findViewById(R.id.inputPassword)
         btnSignIn = findViewById(R.id.button)
         tvSignUp = findViewById(R.id.textViewSignUp)
+        val passwordToggle = R.drawable.custom_show_password
+//        val passwordToggleDrawable = edtPassword.compoundDrawables[0]
 
+        // sign in
         btnSignIn.setOnClickListener {
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
@@ -46,10 +54,30 @@ class Login : AppCompatActivity() {
 
         }
 
+        // sign up
         tvSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        // toggle password
+        edtPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            ContextCompat.getDrawable(this, R.drawable.custom_password_icon), null, ContextCompat.getDrawable(this, passwordToggle), null
+        )
+
+        // Toggle password visibility
+        edtPassword.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                // Check if the click was within the bounds of the drawable
+                if (event.rawX >= (edtPassword.right - edtPassword.compoundDrawables[2].bounds.width())) {
+                    togglePasswordVisibility()
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
+
+
     }
 
     private fun login(email: String, password: String){
@@ -62,5 +90,22 @@ class Login : AppCompatActivity() {
                     Toast.makeText(this@Login, "Either the username or password is incorrect", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    private fun togglePasswordVisibility() {
+        if(isPasswordVisible) {
+            // hide password
+            edtPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            edtPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.custom_password_icon, 0, R.drawable.baseline_visibility_24, 0)
+            isPasswordVisible = false
+        } else {
+            // show password
+            edtPassword.transformationMethod = null
+            edtPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.custom_password_icon, 0, R.drawable.baseline_visibility_off_24, 0)
+            isPasswordVisible = true
+        }
+
+        // move cursor to the end of the text
+        edtPassword.setSelection(edtPassword.text.length)
     }
 }
